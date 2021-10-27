9d82f88bcc6dd95ead2c360b6d422ae0d8d58e4c
Paul Weiß <paul.weiss@fu-berlin.de>
2021-10-28 00:26:44 +0200

    C Code is running somewhat

---
+++ startup.c
@@ -0,0 +1,18 @@
+
+// needed to prevent gcc from optimizing c_entry out.
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+
+
+void c_entry(void) {
+	// here be dragons
+	int a = 0;
+	for (int i = 0; i < 10; ++i) {
+		a += i;
+	}
+	for (;;);
+	// we cannot return, because no stack...
+}
+
+
+#pragma GCC pop_options
