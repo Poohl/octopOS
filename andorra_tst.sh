30d9279a7d0182e3a99e5a42435154580b0c842c
fjezuita <fjezuita@mi.fu-berlin.de>
2021-10-29 12:30:22 +0200

    added andorra-test-skript

---
+++ andorra_tst.sh
@@ -0,0 +1,10 @@
+# test the kernel on andorra using Leo's build of qemu
+# run: $ sh andorra_tst.sh [FU_USERNAME]
+
+# copy kernel to $HOME on andorra
+scp kernel.elf $1@andorra.imp.fu-berlin.de:~/tmp_kernel.elf
+# run kernel then remove it
+ssh $1@andorra.imp.fu-berlin.de \
+'/home/mi/koenigl/Git/qemu-build/arm-softmmu/qemu-system-arm \
+ -nographic -M portux920t -m 64M -kernel tmp_kernel.elf \
+ ; rm tmp_kernel.elf'
