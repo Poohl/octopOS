44b831abdc380d073286657a96aacee8f3ff1d83
fjezuita <fjezuita@mi.fu-berlin.de>
2021-10-30 17:19:43 +0200

    printf demo

--- startup.c
+++ startup.c
@@ -7,6 +7,7 @@
 #pragma GCC push_options
 #pragma GCC optimize ("O0")
 
+/* crucial use of memory*/
 void logo() {
 printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOdooodOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
 printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMNOo;'     ';lONMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
@@ -43,26 +44,25 @@ printf("                                      (c) Paul Weiss & Fabian Jezuita\n"
 printf("*********************************************************************\n");
 }
 
+void printfdemo() {
+	printf("printf demo mode -- PRESS ANY KEY\n");
+	int ret = dbgu_put_byte('#');
+	int c = dbgu_get_byte();	
+	if (c <= 0) {
+		printf("Error %x reading that!\n", c);
+		printf("Here are some other nicely formatted things: %p, %x, %c, %s\n", &printfdemo, 0x12, '3', "45");
+	} else {
+		printf("\nWell done!\nYou entered '%c', acsii index %x \nand it's currently stored at %p in my memory.\n", c, c, &c);
+	}	
+	printf("And heres what happnes with unknown formats:\n %a %b %% %f\n", 1, 2, 3, 4);	
+	printf("done. (press ^a then x to quit)");
+}
 
+/* MAIN */
 void c_entry(void) {
 	dbgu_init();
 	logo();
-	int ret = dbgu_put_byte('#');
-
-	//printf("Please enter a character to get some info:\n");
-//
-	//int c = dbgu_get_byte();
-//
-	//if (c <= 0) {
-	//	printf("Error %x reading that!\n", c);
-	//	printf("Here are some other nicely formatted things: %p, %x, %c, %s\n", &c_entry, 0x12, '3', "45");
-	//} else {
-	//	printf("You entered a %c, acsii index %x and it's currently stored at %p in my memory\n", c, c, &c);
-	//}
-//
-	//printf("And heres what happnes with unknown formats: %a %b %% %f\n", 1, 2, 3, 4);
-//
-	//printf("done");
+	printfdemo();
 }
 
 #pragma GCC pop_options
