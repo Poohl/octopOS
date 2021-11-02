871892603abe406d3b8098963c8f5d44988ac4e0
fjezuita <fjezuita@mi.fu-berlin.de>
2021-11-02 14:03:01 +0100

    demo loop

--- apps/demo.c
+++ apps/demo.c
@@ -4,15 +4,22 @@
 #include "libs/printf.h"
 
 void io_demo() {
+	int c;
 	printf("printf demo mode -- PRESS ANY KEY\n");
-	int ret = dbgu_put_byte('#');
-	int c = dbgu_get_byte();	
-	if (c <= 0) {
-		printf("Error %x reading that!\n", c);
-		printf("Here are some other nicely formatted things: %p, %x, %c, %s\n", &io_demo, 0x12, '3', "45");
-	} else {
-		printf("\nWell done!\nYou entered '%c', acsii index %x \nand it's currently stored at %p in my memory.\n", c, c, &c);
-	}	
+	printf("(press $ to quit)\n");
+	do {
+		int ret = dbgu_put_byte('#');
+		c = dbgu_get_byte();	
+		if (c <= 0) {
+			printf("Error %x reading that!\n", c);
+			printf("Here are some other nicely formatted things: %p, %x, %c, %s\n", &io_demo, 0x12, '3', "45");
+		} else {
+			printf("\nWell done!\nYou entered '%c', acsii index %x \nand it's currently stored at %p in my memory.\n", c, c, &c);
+		}	
+
+	} while (c != '$');
+
+	printf("end of demo loop.\n");
 	printf("And heres what happnes with unknown formats:\n %a %b %% %f\n", 1, 2, 3, 4);	
-	printf("done. (press ^a then x to quit)");
+	printf("done. (press ^a then x to quit qemu)");
 }
