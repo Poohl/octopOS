ca8cd9fa47996164626192cce920506f196eba39
weip00 <weip00@mi.fu-berlin.de>
2021-11-06 12:22:52 +0000

    Add Comments on printf

--- interfaces.h
+++ interfaces.h
@@ -4,6 +4,11 @@
 #include "default.h"
 
 // Utilites, maybe move somewhere else
+/**
+ * Struct to hold the result of multiple sequential IO operations, as we don't have errno
+ * io: number of sucessfull io operations
+ * err: error on the (io+1)th operation or 0 if all requests were sucessfull
+ */
 typedef struct {
 	uint io;
 	int err;
