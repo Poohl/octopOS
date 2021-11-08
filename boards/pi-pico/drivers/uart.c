2f6437669f26fc5c8ebfafcde4b7e7b80abe18d5
Paul Weiß <paul.weiss@fu-berlin.de>
2021-11-08 22:16:12 +0100

    removed unneeded code

--- boards/pi-pico/drivers/uart.c
+++ boards/pi-pico/drivers/uart.c
@@ -116,20 +116,20 @@ unsigned int GET32 ( unsigned int dest) {
 
 int uart_recv ( void )
 {
-    while(1)
-    {
-        if((GET32(UART0_BASE_UARTFR_RW)&(1<<4))==0) break;
-    }
-    return(GET32(UART0_BASE_UARTDR_RW));
+	while(1)
+	{
+		if((GET32(UART0_BASE_UARTFR_RW)&(1<<4))==0) break;
+	}
+	return(GET32(UART0_BASE_UARTDR_RW));
 }
 
 int uart_send ( unsigned int x )
 {
-    while(1)
-    {
-        if((GET32(UART0_BASE_UARTFR_RW)&(1<<5))==0) break;
-    }
-    PUT32(UART0_BASE_UARTDR_RW,x);
+	while(1)
+	{
+		if((GET32(UART0_BASE_UARTFR_RW)&(1<<5))==0) break;
+	}
+	PUT32(UART0_BASE_UARTDR_RW,x);
 	return 0;
 }
 
@@ -160,63 +160,62 @@ sequence_io_status uart_read(uint len, byte* buff) {
 
 //static void uart_flush ( void )
 //{
-    //while(1)
-    //{
-        //if((GET32(UART0_BASE_UARTFR_RW)&(1<<7))!=0) break;
-    //}
+	//while(1)
+	//{
+		//if((GET32(UART0_BASE_UARTFR_RW)&(1<<7))!=0) break;
+	//}
 //}
 
 static void clock_init ( void )
 {
-    PUT32(CLK_SYS_RESUS_CTRL_RW,0);
-    //PUT32(CLK_REF_CTRL_RW,0);
-    //PUT32(CLK_SYS_CTRL_RW,0);
-    PUT32(XOSC_CTRL_RW,0xAA0);      //1 - 15MHZ
-    PUT32(XOSC_STARTUP_RW,47);      //straight from the datasheet
-    PUT32(XOSC_CTRL_SET,0xFAB000);  //enable
-    while(1)
-    {
-        if((GET32(XOSC_STATUS_RW)&0x80000000)!=0) break;
-    }
-    PUT32(CLK_REF_CTRL_RW,2); //XOSC
-    PUT32(CLK_SYS_CTRL_RW,0); //reset/clk_ref
+	PUT32(CLK_SYS_RESUS_CTRL_RW,0);
+	//PUT32(CLK_REF_CTRL_RW,0);
+	//PUT32(CLK_SYS_CTRL_RW,0);
+	PUT32(XOSC_CTRL_RW,0xAA0);      //1 - 15MHZ
+	PUT32(XOSC_STARTUP_RW,47);      //straight from the datasheet
+	PUT32(XOSC_CTRL_SET,0xFAB000);  //enable
+	while(1)
+	{
+		if((GET32(XOSC_STATUS_RW)&0x80000000)!=0) break;
+	}
+	PUT32(CLK_REF_CTRL_RW,2); //XOSC
+	PUT32(CLK_SYS_CTRL_RW,0); //reset/clk_ref
 }
 
 int uart_init ( void )
 {
-    unsigned int ra;
 
-    clock_init();
+	clock_init();
 
-    PUT32(CLK_PERI_CTRL_RW,(1<<11)|(4<<5));
+	PUT32(CLK_PERI_CTRL_RW,(1<<11)|(4<<5));
 
-    PUT32(RESETS_RESET_CLR,(1<<5)); //IO_BANK0
-    while(1)
-    {
-        if((GET32(RESETS_RESET_DONE_RW)&(1<<5))!=0) break;
-    }
-    PUT32(RESETS_RESET_CLR,(1<<22)); //UART0
-    while(1)
-    {
-        if((GET32(RESETS_RESET_DONE_RW)&(1<<22))!=0) break;
-    }
+	PUT32(RESETS_RESET_CLR,(1<<5)); //IO_BANK0
+	while(1)
+	{
+		if((GET32(RESETS_RESET_DONE_RW)&(1<<5))!=0) break;
+	}
+	PUT32(RESETS_RESET_CLR,(1<<22)); //UART0
+	while(1)
+	{
+		if((GET32(RESETS_RESET_DONE_RW)&(1<<22))!=0) break;
+	}
 
-    //GPIO 0 UART0 TX function 2
-    //GPIO 1 UART0 RX function 2
+	//GPIO 0 UART0 TX function 2
+	//GPIO 1 UART0 RX function 2
 
-    //(12000000/(16/115200)) = 6.514
-    //0.514*64 = 32.666
-    PUT32(UART0_BASE_UARTIBRD_RW,6);
-    PUT32(UART0_BASE_UARTFBRD_RW,33);
-    //0 11 1 0 0 0 0
-    //0111 0000
-    PUT32(UART0_BASE_UARTLCR_H_RW,0x70);
-    PUT32(UART0_BASE_UARTCR_RW,(1<<9)|(1<<8)|(1<<0));
+	//(12000000/(16/115200)) = 6.514
+	//0.514*64 = 32.666
+	PUT32(UART0_BASE_UARTIBRD_RW,6);
+	PUT32(UART0_BASE_UARTFBRD_RW,33);
+	//0 11 1 0 0 0 0
+	//0111 0000
+	PUT32(UART0_BASE_UARTLCR_H_RW,0x70);
+	PUT32(UART0_BASE_UARTCR_RW,(1<<9)|(1<<8)|(1<<0));
 
-    PUT32(IO_BANK0_GPIO0_CTRL_RW,2);  //UART
-    PUT32(IO_BANK0_GPIO1_CTRL_RW,2);  //UART
+	PUT32(IO_BANK0_GPIO0_CTRL_RW,2);  //UART
+	PUT32(IO_BANK0_GPIO1_CTRL_RW,2);  //UART
 
-    return(0);
+	return(0);
 }
 
 int debug_put_char(char c) {
@@ -224,10 +223,4 @@ int debug_put_char(char c) {
 }
 int debug_get_char() {
 	return uart_recv();
-}
-sequence_io_status debug_write(uint len, const byte* data) {
-	return uart_write(len, data);
-}
-sequence_io_status debug_read(uint len, byte* buff) {
-	return uart_read(len, buff);
 }
