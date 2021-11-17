02d40a295fa0b73b41b9c93a14777ea84b5c2821
fjezuita <fjezuita@mi.fu-berlin.de>
2021-11-17 18:36:37 +0100

    code injection poc

--- boards/portux/startup.c
+++ boards/portux/startup.c
@@ -9,10 +9,39 @@
 #pragma GCC optimize ("O0")
 
 extern void init_stacks();
+extern void RANDO_CODE ();
 
 void __attribute__((interrupt ("UNDEF"))) illegal_instr_hand() {
 	printf("Illegal instruction\r\n");
 }
+
+int powHex(int e) {
+	int ret = 1;
+	for (int i = 0; i < e; i++) ret *= 16;
+	return ret;
+}
+
+int toInt(char* hexStr) {
+	int ret = 0;
+	int asciiOffset;
+
+	for (int i = 0; i < 8; i++) {
+		if (hexStr[i] < 58) 
+			asciiOffset = 48;
+		else 
+			asciiOffset = 87;
+		ret += (hexStr[i] - asciiOffset) * powHex(7 - i);
+	}
+	return ret;
+}
+
+void inject_code() {
+	int* payload = (int *) 0x26ff00;
+	payload[0] = toInt("775f0074");			// illegal instruction
+
+	RANDO_CODE();
+}
+
 
 /* MAIN */
 void c_entry(void) {
@@ -21,10 +50,15 @@ void c_entry(void) {
 	init_stacks();
 	set_undef_instr_vector(&illegal_instr_hand);
 	switch_vector_table();
+
+	inject_code();
+
+	/*
 	asm(".word 0x775f0074" : : : "memory");
-	/* run stuff */
+	/* run stuff 
 	print_banner();
 	io_demo();
+	*/
 
 }
 
