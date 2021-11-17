3d0cb263f95b48b0eb37243cc4663698ff488d70
Paul Weiß <paul.weiss@fu-berlin.de>
2021-11-18 00:47:11 +0100

    pi-pico exception handling

---
+++ boards/pi-pico/board.h
@@ -0,0 +1,10 @@
+
+#define SRAM_BASE 0x10000000
+#define VECTORS_BASE 0x10000100
+
+#define INIT_MAIN_STACK 0x20001000
+#define INIT_PROCESS_STACK 0x20002000
+
+#define SYS_CTL_VTOR 0xE000ED08
+
+void _start(void);
